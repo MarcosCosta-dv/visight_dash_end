8c640d229579fa998d636b53afa008a10972447b
Lucas Simidu <lucas.simidu@outlook.com>
2024-10-23 00:23:55 -0300

    FIX/ Reduzindo a Navbar a um Component
    Co-Authored-By: Marcos Costa <79596734+MarcosCosta-dv@users.noreply.github.com>

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -1,5 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarContainer = document.getElementById('navbar-container');
+  
+  fetch('assets/components/navbar.html')
+      .then(response => response.text())
+      .then(data => {
+          navbarContainer.innerHTML = data;
+      })
+      .catch(error => console.error('Erro ao carregar a navbar:', error));
+  });
+
 document.getElementById('toggleBtn').addEventListener('click', function () {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.toggle('active');
   });
-  
+
